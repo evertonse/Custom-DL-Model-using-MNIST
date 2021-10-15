c8db481e161aab4896b062ee2b5a50fda1d8da65
Everton Jr <69195288+evertonse@users.noreply.github.com>
2021-10-15 01:53:50 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -9,19 +9,27 @@ def test_prediction(index, data, model:Model):
 	prediction = (y_predict == y_predict.max()).astype(int)
 
 	guess = list(prediction).index(1)
-
 	label = data["outputs"][index]
+	ground_truth = list(label).index(1)
+
 	print("Label: ", label)
 	print("Prediction: ", prediction)
 
-	plt.gray()
-	plt.title("Modelo acha que é: " + str(guess))
-	plt.imshow( current_image.reshape((28, 28)) * 255, interpolation='nearest')
-	#plt.show()
-	plt.draw()
-	plt.waitforbuttonpress(0) # this will wait for indefinite time		
-	plt.close()
+	# Opção de desobrigar de fornecer label correto, para quando formor utilizar paint
+	if len(label) < 10:
+		label = "made on paint"
+		ground_truth = " paint"
 
+	print("Label: ", label)
+	print("Prediction: ", prediction)
+	plt.gray()
+	plt.title("Model thinks it is: " + str(guess) + "\nGround truth: " + str(ground_truth))
+	plt.imshow( current_image.reshape((28, 28)) * 255, interpolation='nearest')
+	plt.xticks([])
+	plt.yticks([0])
+	plt.show()
+
+	
 def __main__():
 
 
@@ -44,7 +52,8 @@ def __main__():
 	}
 
 	learning_rate = 0.035
-	epochs = 1
+	# 3 épocas é bem pouco, mas já chega a 95% de acurácia, podendo chega a 98.9% com mais treinamento
+	epochs = 3
 	model_filename = "model_128x128"
 
 	model  = Model((784,128,128,10), activation="sigmoid", verbose=0, wr=(-0.5,0.5))
@@ -63,19 +72,18 @@ def __main__():
 
 	model.print()
 
-
 	model.save("./models/" + model_filename)
 	print("> model saved in: ",model_filename)
 
 
 	while True:
-		index = input("> Escolha uma imagem entre [0, 10k): ")
+		index = input("> Escolha uma imagem entre [0, 28'000): ")
 		if not index.isnumeric():
 			break
 		try:
 			test_prediction(int(index),test_data, model)
 		except:
-			print("> Imagem deve ser entre 1 e 10'000\n")
+			print("> Imagem deve ser entre 1 e 28'000\n")
 		continue
 
 __main__()
